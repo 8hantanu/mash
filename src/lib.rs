f07c3c1b4f4dfa6f056eb16996d096ae8ebc1c7c
Shantanu Mishra <dev@8hantanu.net>
2024-11-27 22:54:15 +0530

    Make it a lib

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+//! `mash-up` is a Rust library that provides the dependencies for building web apps
+//! using the MASH stack: Maud, Axum, SQLx, and HTMX. Instead of managing these
+//! dependencies manually, you can simply include this crate in your project to
+//! import all the necessary dependencies for web development.
+
+pub fn placeholder() {
+    // This is just a placeholder to make the crate compile.
+}
+
